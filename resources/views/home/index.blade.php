f5c82fa7a8fed9602231502c49d116aa678c5004
victorchukwuemeka <chukwuemekavictor693@gmail.com>
2023-06-03 11:07:39 +0100

    cuurent change

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,12 +7,12 @@
   @foreach( $viewData["farms"] as $farm)
   @if( $farm->get_id() === $product->get_farms_id() )
   <div class="col">
-    <div class="card text-dark bg-light" style="width: 18rem";>
+    <div class="card text-dark bg-light">
 
            <div class="card-header">  {{ $product->get_title() }} </div>
            <div class="card-body">
           <img src="{{ asset('/image/'.$product->get_products_files()) }}"
-           class="card-img-top img-fluid rounded" alt="...">
+           class="card-img-top img-fluid rounded" alt="card image">
           <h5 class="card-title">Location: {{ $farm->get_location() }}</h5>
         <p class="card-text">Contact:  {{ $farm->get_mobile() }} </p>
         <a href="{{ $farm->get_link()}}" class="btn btn-primary">socials</a>
